4f62b96960a2d51685b8b97e78fd631078ba1a38
Jkerbin <2964535636@qq.com>
2025-01-30 12:49:49 +0800

    singleton pattern

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, shell, BrowserWindow, ipcMain, Tray } from 'electron'
+import { app, shell, BrowserWindow, ipcMain, Tray, Menu } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
@@ -46,6 +46,18 @@ function createWindow(): void {
         mainWindow.show();
       }
     });
+    // Create a tray right-click menu
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'quit',
+        click: () => {
+          app.quit();
+        }
+      }
+    ]);
+
+    // Set the tray right-click menu
+    tray.setContextMenu(contextMenu);
   } catch (error) {
     console.error('tray fail:', error);
   }
@@ -77,119 +89,137 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  ipcMain.handle('reload', () => {
+// Singleton lock check
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    // focus on the window of the first instance
     if (mainWindow) {
-      mainWindow.reload(); // reload
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore();
+      }
+      mainWindow.show();
+      mainWindow.focus();
     }
   });
 
-  ipcMain.handle('exit', () => {
-    if (mainWindow) {
-      app.quit(); // quit
-    }
-  });
-
-  ipcMain.handle('close', () => {
-    if (mainWindow) {
-      mainWindow.hide() // hide window
-    }
-  });
-
-  ipcMain.handle('maximize', () => {
-    if (mainWindow) {
-      mainWindow.maximize(); // maximize
-    }
-  });
-
-  ipcMain.handle('restore', () => {
-    if (mainWindow) {
-      mainWindow.restore(); // maximize
-    }
-  });
-
-  ipcMain.handle('minimize', () => {
-    if (mainWindow) {
-      mainWindow.minimize(); // minimize
-    }
-  });
-
-  // Test creating docker container
-  ipcMain.handle('run-docker-test', () => {
-    const { spawn } = require('child_process');
-    const command = 'docker';
-    const args = [
-      'run',
-      '--runtime=nvidia',
-      '--gpus', 'all',
-      '--name', 'DeepSeek-Deployment-Webui',
-      '-v', `${process.env.USERPROFILE}/.cache/huggingface:/root/.cache/huggingface`,
-      '-v', 'deepseek-model-volume:/deepseek-ai',
-      '-p', '1229:8000',
-      '--ipc=host',
-      'vllm/vllm-openai:latest',
-      '--model', '/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B',
-      '--gpu_memory_utilization', '1.0',
-      '--max_model_len', '8192'
-    ];
-
-    const child = spawn(command, args);
-
-    child.stdout.on('data', (data) => {
-      mainWindow.webContents.send('docker-run-info', data.toString());
-    });
-
-    child.stderr.on('data', (data) => {
-      mainWindow.webContents.send('docker-run-info', data.toString());
-      if (data.toString().includes('Uvicorn running on http://0.0.0.0:8000 (Press CTRL+C to quit)')) {
-        console.log('ok');
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(() => {
+    ipcMain.handle('reload', () => {
+      if (mainWindow) {
+        mainWindow.reload(); // reload
       }
     });
 
-    child.on('close', (code) => {
-      console.log(`exit code ${code}`);
+    ipcMain.handle('exit', () => {
+      if (mainWindow) {
+        app.quit(); // quit
+      }
     });
-  });
 
-  ipcMain.handle('get-graphics-info', async () => {
-    const si = require('systeminformation');
-    try {
-      const graphics = await si.graphics();
-      console.log('Graphics Information:');
-      graphics.controllers.forEach((controller, index) => {
-        console.log(`Controller ${index + 1}:`);
-        console.log(`  Model: ${controller.model}`);
-        console.log(`  Vendor: ${controller.vendor}`);
-        console.log(`  VRAM: ${controller.vram} MB`);
-        console.log(`  Bus: ${controller.bus}`);
-        console.log('--------------------------');
+    ipcMain.handle('close', () => {
+      if (mainWindow) {
+        mainWindow.hide() // hide window
+      }
+    });
+
+    ipcMain.handle('maximize', () => {
+      if (mainWindow) {
+        mainWindow.maximize(); // maximize
+      }
+    });
+
+    ipcMain.handle('restore', () => {
+      if (mainWindow) {
+        mainWindow.restore(); // maximize
+      }
+    });
+
+    ipcMain.handle('minimize', () => {
+      if (mainWindow) {
+        mainWindow.minimize(); // minimize
+      }
+    });
+
+    // Test creating docker container
+    ipcMain.handle('run-docker-test', () => {
+      const { spawn } = require('child_process');
+      const command = 'docker';
+      const args = [
+        'run',
+        '--runtime=nvidia',
+        '--gpus', 'all',
+        '--name', 'DeepSeek-Deployment-Webui',
+        '-v', `${process.env.USERPROFILE}/.cache/huggingface:/root/.cache/huggingface`,
+        '-v', 'deepseek-model-volume:/deepseek-ai',
+        '-p', '1229:8000',
+        '--ipc=host',
+        'vllm/vllm-openai:latest',
+        '--model', '/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B',
+        '--gpu_memory_utilization', '1.0',
+        '--max_model_len', '8192'
+      ];
+
+      const child = spawn(command, args);
+
+      child.stdout.on('data', (data) => {
+        mainWindow.webContents.send('docker-run-info', data.toString());
       });
-    } catch (error) {
-      console.error('Error fetching graphics information:', error);
-    }
+
+      child.stderr.on('data', (data) => {
+        mainWindow.webContents.send('docker-run-info', data.toString());
+        if (data.toString().includes('Uvicorn running on http://0.0.0.0:8000 (Press CTRL+C to quit)')) {
+          console.log('ok');
+        }
+      });
+
+      child.on('close', (code) => {
+        console.log(`exit code ${code}`);
+      });
+    });
+
+    ipcMain.handle('get-graphics-info', async () => {
+      const si = require('systeminformation');
+      try {
+        const graphics = await si.graphics();
+        console.log('Graphics Information:');
+        graphics.controllers.forEach((controller, index) => {
+          console.log(`Controller ${index + 1}:`);
+          console.log(`  Model: ${controller.model}`);
+          console.log(`  Vendor: ${controller.vendor}`);
+          console.log(`  VRAM: ${controller.vram} MB`);
+          console.log(`  Bus: ${controller.bus}`);
+          console.log('--------------------------');
+        });
+      } catch (error) {
+        console.error('Error fetching graphics information:', error);
+      }
+    })
+
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron')
+
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
+
+    createWindow()
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
   })
-
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
